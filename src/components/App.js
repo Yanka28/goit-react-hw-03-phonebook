8782e7f9b196a8a581a7294bb5f6a9e8e4887b5c
Yanka28 <yanachepeluk@ukr.net>
2023-09-01 20:05:04 +0300

    comm1

---
+++ src/components/App.js
@@ -0,0 +1,83 @@
+import { Layout } from './Layout/Layout';
+import { ContactForm } from './ContactForm/ContactForm';
+import { FilterContacts } from './FilterContacts/FilterContacts';
+import { ContactsList } from './ContactsList/ContactsList';
+import { GlobalStyle } from './GlobalStyle';
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      this.setState({
+        contacts: JSON.parse(savedContacts),
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  addContact = object =>
+    this.setState(prevState => ({
+      contacts: [
+        ...prevState.contacts,
+        { id: nanoid(), name: object.name, number: object.number },
+      ],
+    }));
+
+  onDelete = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  getExistingContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  handleChangeFilter = e => {
+    this.setState({
+      filter: e.target.value,
+    });
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    return (
+      <Layout>
+        <h1>Phonebook</h1>
+        <ContactForm contacts={contacts} addContact={this.addContact} />
+        <h2>Contacts</h2>
+        <FilterContacts
+          filter={filter}
+          handleChangeFilter={this.handleChangeFilter}
+        />
+        <ContactsList
+          contacts={this.getExistingContacts()}
+          onDelete={this.onDelete}
+        />
+
+        <GlobalStyle />
+      </Layout>
+    );
+  }
+}
